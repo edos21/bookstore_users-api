ced49009b75d4b573d3f42dba6931c068d9142ee
eduardo sanchez <eduardo.sanchez@pedidosya.com>
2020-09-07 19:20:48 -0300

    adds user and utils for managing errors

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,11 +3,25 @@ package users
 import (
 	"net/http"
 
+	"github.com/edos21/bookstore_users-api/domain/users"
+	"github.com/edos21/bookstore_users-api/services"
+	"github.com/edos21/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
