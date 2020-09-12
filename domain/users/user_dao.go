f9c42cc3e720c51cd3d1228b1c62c0275160b08c
eduardo sanchez <eduardo.sanchez@pedidosya.com>
2020-09-12 12:50:40 -0300

    new dto and dao for users access

---
+++ domain/users/user_dao.go
@@ -0,0 +1,11 @@
+package users
+
+import "github.com/edos21/bookstore_users-api/utils/errors"
+
+func (u User) Get() *errors.RestErr {
+	return nil
+}
+
+func (u User) Save() *errors.RestErr {
+	return nil
+}
